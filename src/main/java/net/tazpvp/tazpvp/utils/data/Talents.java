ee28a56c4fd0ba5426fd4997e24fd0009e00a56d
rownox <rownox.ik@gmail.com>
2022-11-21 22:38:39 -0500

    Talent List

--- src/main/java/net/tazpvp/tazpvp/utils/data/Talents.java
+++ src/main/java/net/tazpvp/tazpvp/utils/data/Talents.java
@@ -8,6 +8,44 @@ import java.io.Serializable;
 public class Talents implements Serializable {
     @Getter
     @Setter
-    private boolean Perk1 = false;
-
+    private boolean Revenge = false;
+    @Getter
+    @Setter
+    private boolean Moist = false;
+    @Getter
+    @Setter
+    private boolean Resilient = false;
+    @Getter
+    @Setter
+    private boolean Excavator = false;
+    @Getter
+    @Setter
+    private boolean Architect = false;
+    @Getter
+    @Setter
+    private boolean Hunter = false;
+    @Getter
+    @Setter
+    private boolean Cannibal = false;
+    @Getter
+    @Setter
+    private boolean Agile = false;
+    @Getter
+    @Setter
+    private boolean Harvester = false;
+    @Getter
+    @Setter
+    private boolean Necromancer = false;
+    @Getter
+    @Setter
+    private boolean Blessed = false;
+    @Getter
+    @Setter
+    private boolean Glider = false;
+    @Getter
+    @Setter
+    private boolean Proficient = false;
+    @Getter
+    @Setter
+    private boolean Medic = false;
 }
