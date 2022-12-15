52c03a025110265b8fbf35b0dbf1e6a787c6cdb3
Ntdi <79174456+n-tdi@users.noreply.github.com>
2022-12-15 17:15:07 -0500

    fikis bokis

---
+++ src/main/java/net/tazpvp/tazpvp/listeners/InventoryClick.java
@@ -0,0 +1,12 @@
+package net.tazpvp.tazpvp.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryClick implements Listener {
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+
+    }
+}
--- src/main/java/net/tazpvp/tazpvp/utils/PDCUtil.java
+++ src/main/java/net/tazpvp/tazpvp/utils/PDCUtil.java
@@ -8,23 +8,21 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.javatuples.Pair;
 
-import java.util.UUID;
-
 public class PDCUtil {
     @Getter
     private static NamespacedKey npcKey = new NamespacedKey(Tazpvp.getInstance(), "npc-key");
 
-    public static void setPDC(Entity e, NamespacedKey key, UUID value) {
-        e.getPersistentDataContainer().set(key, PersistentDataType.STRING, value.toString());
+    public static void setPDC(Entity e, NamespacedKey key, String value) {
+        e.getPersistentDataContainer().set(key, PersistentDataType.STRING, value);
     }
 
-    public static Pair<Boolean, UUID> hasPDC(Entity e, NamespacedKey key) {
+    public static Pair<Boolean, String> hasPDC(Entity e, NamespacedKey key) {
         PersistentDataContainer container  = e.getPersistentDataContainer();
 
         if (container.has(key, PersistentDataType.STRING)) {
-            return Pair.with(true, UUID.fromString(container.get(key, PersistentDataType.STRING)));
+            return Pair.with(true, (container.get(key, PersistentDataType.STRING)));
         }
-        return Pair.with(false, UUID.randomUUID());
+        return Pair.with(false, "");
     }
 
 }
