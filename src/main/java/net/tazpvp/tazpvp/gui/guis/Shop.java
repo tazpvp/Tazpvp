f614adc4572863c92ffaf0b8cfa74d90f24b6a26
rownox <rownox.ik@gmail.com>
2022-12-15 11:12:53 -0500

    Rgb

--- src/main/java/net/tazpvp/tazpvp/gui/guis/Shop.java
+++ src/main/java/net/tazpvp/tazpvp/gui/guis/Shop.java
@@ -32,10 +32,14 @@
 
 package net.tazpvp.tazpvp.gui.guis;
 
+import net.tazpvp.tazpvp.Tazpvp;
 import net.tazpvp.tazpvp.gui.GuiUtils;
+import net.tazpvp.tazpvp.utils.data.DataTypes;
+import net.tazpvp.tazpvp.utils.data.PersistentData;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import world.ntdi.nrcore.utils.gui.Button;
 import world.ntdi.nrcore.utils.gui.GUI;
 import world.ntdi.nrcore.utils.item.builders.ItemBuilder;
@@ -45,15 +49,30 @@ public class Shop extends GUI {
     public Shop(Player p) {
         super("Shop", 4);
 
-        setButton("test", GuiUtils.makeLore("test", "test 2"), Material.STONE, 1, p);
+        setButton("test", GuiUtils.makeLore("test", "test 2"), Material.STONE, 1, p, 30);
 
         open(p);
     }
 
-    private void setButton(String name, String[] lore, Material mat, int amount, Player p) {
+    private void setButton(String name, String[] lore, Material mat, int amount, Player p, int cost) {
         addButton(Button.create(ItemBuilder.of(mat, amount).name(name).lore(lore).build(), (e) -> {
-            p.getInventory().addItem(new ItemStack(mat, amount));
+            if (PersistentData.getInt(p, DataTypes.COINS) >= cost) {
+                PersistentData.remove(p, DataTypes.COINS, cost);
+                p.getInventory().addItem(new ItemStack(mat, amount));
+                p.sendMessage("you purchased" + name);
+            }
         }), GuiUtils.slotNum);
         GuiUtils.calcSlot();
+    }
+
+    private void setRgbButton() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if () {
+
+                }
+            }
+        }.runTaskTimer(Tazpvp.getInstance(), 20, 20);
     }
 }
