d98fb24b108668cb5929dd344596a29e8e35142e
Ntdi <79174456+n-tdi@users.noreply.github.com>
2022-12-16 21:50:05 -0500

    cahe

--- src/main/java/net/tazpvp/tazpvp/commands/GuildCommandFunction.java
+++ src/main/java/net/tazpvp/tazpvp/commands/GuildCommandFunction.java
@@ -1,11 +1,16 @@
 package net.tazpvp.tazpvp.commands;
 
 import net.tazpvp.tazpvp.guild.Guild;
+import net.tazpvp.tazpvp.utils.data.DataTypes;
 import net.tazpvp.tazpvp.utils.data.GuildData;
+import net.tazpvp.tazpvp.utils.data.PersistentData;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import world.ntdi.nrcore.utils.command.CommandCore;
 import world.ntdi.nrcore.utils.command.CommandFunction;
+
+import java.util.UUID;
 
 public class GuildCommandFunction extends CommandCore implements CommandFunction {
 
@@ -19,6 +24,8 @@ public class GuildCommandFunction extends CommandCore implements CommandFunction
         if (commandSender instanceof Player p) {
             Guild g = new Guild("my favoiret guild", p.getUniqueId());
             GuildData.setGuild(g.getID(), g);
+
+            Bukkit.getLogger().info(GuildData.getGuild(UUID.fromString(PersistentData.getString(p, DataTypes.GUILD_ID))).getName());
         }
     }
 }
