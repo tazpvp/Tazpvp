656c027e208dd899763bc7e0857b489439472dfa
rownox <rownox.ik@gmail.com>
2022-12-16 21:52:25 -0500

    guis fixkes

--- src/main/java/net/tazpvp/tazpvp/gui/guis/Talents.java
+++ src/main/java/net/tazpvp/tazpvp/gui/guis/Talents.java
@@ -34,15 +34,20 @@ package net.tazpvp.tazpvp.gui.guis;
 
 import net.tazpvp.tazpvp.utils.data.PersistentData;
 import net.tazpvp.tazpvp.utils.enums.CC;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import world.ntdi.nrcore.utils.gui.Button;
 import world.ntdi.nrcore.utils.gui.GUI;
 import world.ntdi.nrcore.utils.item.builders.ItemBuilder;
+import world.ntdi.nrcore.utils.item.builders.PotionBuilder;
+
+import java.awt.*;
 
 public class Talents extends GUI {
 
@@ -55,32 +60,32 @@ public class Talents extends GUI {
 
         fill(0, 4*9, ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE, 1).name(" ").build());
 
-        setButton(p, 10, 8, "Revenge", "Set the player who killed you on fire.", TALENT.isRevenge());
-        setButton(p, 11, 9, "Moist", "You can no longer be set on fire.", TALENT.isRevenge());
-        setButton(p, 12, 12, "Resilient", "Gain 2 absorption hearts on kill.", TALENT.isRevenge());
-        setButton(p, 13, 8, "Excavator", "Mining gives you experience.", TALENT.isRevenge());
-        setButton(p, 14, 6, "Architect", "A chance to reclaim the block you placed.", TALENT.isRevenge());
-        setButton(p, 15, 8, "Hunter", "A chance to reclaim the arrow you shot.", TALENT.isRevenge());
-        setButton(p, 16, 9, "Cannibal", "Replenish your hunger on kill.", TALENT.isRevenge());
+        setButton(p, 10, Material.NETHERITE_SWORD, 8, "Revenge", "Set the player who killed you on fire.", TALENT.isRevenge());
+        setButton(p, 11, Material.WATER_BUCKET, 9, "Moist", "You can no longer be set on fire.", TALENT.isRevenge());
+        setButton(p, 12, Material.SHIELD, 12, "Resilient", "Gain 2 absorption hearts on kill.", TALENT.isRevenge());
+        setButton(p, 13, Material.GOLDEN_PICKAXE,8, "Excavator", "Mining gives you experience.", TALENT.isRevenge());
+        setButton(p, 14, Material.CRAFTING_TABLE,6, "Architect", "A chance to reclaim the block you placed.", TALENT.isRevenge());
+        setButton(p, 15, Material.BOW,8, "Hunter", "A chance to reclaim the arrow you shot.", TALENT.isRevenge());
+        setButton(p, 16, Material.ROTTEN_FLESH,9, "Cannibal", "Replenish your hunger on kill.", TALENT.isRevenge());
 
-        setButton(p, 19, 14, "Agile", "Gain a speed boost on kill.", TALENT.isRevenge());
-        setButton(p, 20, 11, "Harvester", "Better chance that players drop heads.", TALENT.isRevenge());
-        setButton(p, 21, 15, "Necromancer", "Gain more from player coffins.", TALENT.isRevenge());
-        setButton(p, 22, 20, "Blessed", "A chance of getting a gold apple from a kill.", TALENT.isRevenge());
-        setButton(p, 23, 6, "Glider", "The launch pad pushes you further.", TALENT.isRevenge());
-        setButton(p, 24, 9, "Proficient", "Gain experience from duels.", TALENT.isRevenge());
-        setButton(p, 25, 10, "Medic", "Heal nearby guild mates on kill.", TALENT.isRevenge());
+        setButton(p, 19, Material.FEATHER,14, "Agile", "Gain a speed boost on kill.", TALENT.isRevenge());
+        setButton(p, 20, Material.SHEARS,11, "Harvester", "Better chance that players drop heads.", TALENT.isRevenge());
+        setButton(p, 21, Material.NETHERITE_HOE,15, "Necromancer", "Gain more from player coffins.", TALENT.isRevenge());
+        setButton(p, 22, Material.GOLDEN_APPLE,20, "Blessed", "A chance of getting a golden apple from a kill.", TALENT.isRevenge());
+        setButton(p, 23, Material.ELYTRA,6, "Glider", "The launch pad pushes you further.", TALENT.isRevenge());
+        setButton(p, 24, Material.EXPERIENCE_BOTTLE,9, "Proficient", "Gain experience from duels.", TALENT.isRevenge());
+        setButton(p, 25, PotionBuilder.of(PotionBuilder.PotionType.SPLASH).setColor(Color.PURPLE).build().getType(),10, "Medic", "Heal nearby guild mates on kill.", TALENT.isRevenge());
 
         open(p);
     }
 
-    private void setButton(Player p, int slot, int cost, String name, String lore, boolean completed) {
+    private void setButton(Player p, int slot, Material mat, int cost, String name, String lore, boolean completed) {
 
         String complete = completed ? CC.GREEN + "Active" : CC.RED + "Inactive";
-        Material mat = completed ? Material.ENCHANTED_BOOK : Material.WRITABLE_BOOK;
 
-        addButton(Button.create(ItemBuilder.of(mat, 1).name(CC.AQUA + name).lore(CC.DARK_AQUA + "" + CC.BOLD + lore, " ",CC.GRAY + "Cost: " + cost + " Shards", " ", complete).build(), (e) -> {
+        addButton(Button.create(ItemBuilder.of(mat, 1).name(CC.AQUA + name).lore(CC.DARK_AQUA + "" + CC.BOLD + lore, " ",CC.GRAY + "Cost: " + cost + " Shards", " ", complete).flag(ItemFlag.HIDE_POTION_EFFECTS).flag(ItemFlag.HIDE_ATTRIBUTES).build(), (e) -> {
             for (ItemStack i : p.getInventory()) {
+                if (i == null) continue;
                 if (i.getType() == Material.AMETHYST_SHARD) {
                     shardCount = shardCount + i.getAmount();
                 }
@@ -88,6 +93,7 @@ public class Talents extends GUI {
             if (shardCount >= cost) {
                 for (int n = 0 ; n < cost ; n++) {
                     for (ItemStack i : p.getInventory()) {
+                        if (i == null) continue;
                         if (i.getType() == Material.AMETHYST_SHARD) {
                             if (i.getAmount() >= 2) {
                                 i.setAmount(i.getAmount() - 1);
