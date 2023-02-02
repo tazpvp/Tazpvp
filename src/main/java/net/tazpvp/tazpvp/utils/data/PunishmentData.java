d8202f6795d9eda03c3e07dcf698afff55072bab
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-02-01 21:21:14 -0500

    fixed rownox's code part 123958

--- src/main/java/net/tazpvp/tazpvp/utils/data/PunishmentData.java
+++ src/main/java/net/tazpvp/tazpvp/utils/data/PunishmentData.java
@@ -43,17 +43,38 @@ public class PunishmentData {
     public static final String NAME = "punishments";
     public static final String ID = "id";
 
-    public static void addPunishmentTime(final UUID id, final PunishTypes type, final int time) {
-        if (!isPunished(type, id)) return;
+    public static void punish(@Nonnull final UUID uuid, @Nonnull final PunishTypes type, final long timestamp) {
+        if (!isInTable(uuid)) {
+            SQLHelper.initializeValues(NAME, "ID, TIME, MUTED, BANNED", "'" + uuid + "', " + timestamp + ", false, false");
+            SQLHelper.updateValue(NAME, ID, uuid.toString(), type.columnName, true);
+            return;
+        }
 
+        SQLHelper.deleteRow(NAME, ID, uuid.toString());
+        punish(uuid, type, timestamp);
     }
 
-    public static void setPunishTime(final UUID id, final PunishTypes type) {
-        SQLHelper.initializeValues(NAME, "ID, TIME, MUTED, BANNED", "'" + id.toString() + "', '" + 1000 + "', '", false, false);
+    public static void unpunish(@Nonnull final UUID uuid) {
+        if (isInTable(uuid)) {
+            SQLHelper.deleteRow(NAME, ID, uuid.toString());
+        }
+    }
+
+    public static long timeLeft(@Nonnull final UUID uuid) {
+        if (!isInTable(uuid))
+            throw new IllegalArgumentException(uuid + " is not in table! Should check with `isInTable()` first");
+
+        return SQLHelper.getInt(NAME, ID, uuid.toString(), PunishTypes.TIME.getColumnIndex());
     }
 
     public static boolean isPunished(PunishTypes type, UUID id) {
-        return SQLHelper.getBool(NAME, ID, id.toString(), type.columnIndex);
+        if (!isInTable(id)) return false; // Check if in table
+        if (type == PunishTypes.MUTE && isPunished(PunishTypes.BAN, id)) return true; // Check if muted when banned
+        return SQLHelper.getBool(NAME, ID, id.toString(), type.columnIndex); // Final check for punishment
+    }
+
+    public static boolean isInTable(@Nonnull final UUID uuid) {
+        return SQLHelper.ifRowExists(NAME, ID, uuid.toString());
     }
 
     public enum PunishTypes {
