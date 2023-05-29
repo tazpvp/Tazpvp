d739c41394d6cbf3503465b0c66fef94d6984a0e
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-05-29 18:01:28 -0400

    god is great

--- src/main/java/net/tazpvp/tazpvp/utils/player/PlayerWrapper.java
+++ src/main/java/net/tazpvp/tazpvp/utils/player/PlayerWrapper.java
@@ -75,7 +75,8 @@ public class PlayerWrapper {
      * Hide the player from ALL other players.
      */
     public void showPlayer() {
-        getHiddenFrom().forEach(uuid -> {
+        List<UUID> hiddenFrom = getHiddenFrom();
+        hiddenFrom.forEach(uuid -> {
             Player target = Bukkit.getPlayer(uuid);
             if (target != null) {
                 showPlayer(target);
@@ -98,11 +99,9 @@ public class PlayerWrapper {
     }
 
     public void showFromPlayer() {
-        getHiddenFrom().forEach(uuid -> {
-            Player target = Bukkit.getPlayer(uuid);
-            if (target != null ){
-                PlayerWrapper.getPlayer(target).showPlayer(getPlayer());
-            }
+        Bukkit.getOnlinePlayers().forEach(uuid -> {
+            PlayerWrapper wrapper = PlayerWrapper.getPlayer(uuid);
+            wrapper.showPlayer(getPlayer());
         });
     }
 
