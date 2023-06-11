a4e92fcea7ba758ef668072c758437f48bd7a743
rownox <rownox.ik@gmail.com>
2023-06-11 18:51:19 -0400

    a

--- src/main/java/net/tazpvp/tazpvp/utils/passive/Holograms.java
+++ src/main/java/net/tazpvp/tazpvp/utils/passive/Holograms.java
@@ -12,7 +12,7 @@ public class Holograms {
     public static List<Hologram> holograms = new ArrayList<>();
     public static void holograms() {
         holograms.add(new Hologram("Welcome", new Location(Bukkit.getWorld("arena"), 0.5, 99, 26.5), false));
-        holograms.add(new Hologram("AFK PIT", new Location(Bukkit.getWorld("arena"), 14, 99, 6), false));
+        holograms.add(new Hologram("AFK PIT", new Location(Bukkit.getWorld("arena"), 14, 99, 7), false));
     }
 
     public static void removeHolograms() {
