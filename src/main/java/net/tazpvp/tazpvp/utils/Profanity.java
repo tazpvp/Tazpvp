8fac57477ac29784fa47c03f041279b482d0a641
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-06-11 11:54:57 -0400

    i did smoething mayb

--- src/main/java/net/tazpvp/tazpvp/utils/Profanity.java
+++ src/main/java/net/tazpvp/tazpvp/utils/Profanity.java
@@ -10,4 +10,31 @@ public class Profanity {
 
         return filter.filterBadWords(string);
     }
+
+    public boolean containsNaughtyVocabulary(String phrase) {
+        phrase = phrase.toLowerCase()
+                .replaceAll("1", "i")
+                .replaceAll("!", "i")
+                .replaceAll("@", "a")
+                .replaceAll("#", "")
+                .replaceAll("\\$", "s")
+                .replaceAll("%", "")
+                .replaceAll("3", "e")
+                .replaceAll("2", "s")
+                .replaceAll("4", "a")
+                .replaceAll("\\^", "")
+                .replaceAll("\\.", "")
+                .replaceAll("\\-", "")
+                .replaceAll("6", "g")
+                .replaceAll("7", "t")
+                .replaceAll("&", "b")
+                .replaceAll("8", "b")
+                .replaceAll("0", "o")
+                .replaceAll(" ", "")
+                .replaceAll("\\*", "")
+                .replaceAll("~", "")
+                .replaceAll("`", "");
+
+        return false; // TODO: Finish
+    }
 }
