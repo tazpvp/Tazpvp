6f07459462e51d6f3341a9046a36b48f7073e10d
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-06-13 15:18:41 +0100

    my politcal opinions don't matter.

--- src/main/java/net/tazpvp/tazpvp/items/azureVapor.java
+++ src/main/java/net/tazpvp/tazpvp/items/azureVapor.java
@@ -1,4 +1,4 @@
-package net.tazpvp.tazpvp.item.items;
+package net.tazpvp.tazpvp.items;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -6,7 +6,14 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import world.ntdi.nrcore.utils.item.custom.CustomItem;
 
 public class azureVapor extends CustomItem {
-
+    /*
+        Azure vapor? More like azuremines, haha get it bc rownox
+        failed at making a server named azuremines but still wanted
+        to use the word Azure because it's a cool word, I agree with
+        this as azure is enjoyable to look at and read, but reminds
+        me of Azure cloud services from microsoft which are unreliable
+        and quite frankly, shit.
+     */
     public azureVapor() {
         super("Azure Vapor", new String[]{"Extinguish Flames"}, Material.BLUE_ORCHID);
     }
