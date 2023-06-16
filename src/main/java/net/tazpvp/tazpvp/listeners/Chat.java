911918957516f436fcc156aa1005111da7126e59
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-06-16 23:38:39 +0200

    guild space

--- src/main/java/net/tazpvp/tazpvp/listeners/Chat.java
+++ src/main/java/net/tazpvp/tazpvp/listeners/Chat.java
@@ -66,7 +66,7 @@ public class Chat implements Listener {
 
 
 
-        String format = "&GRAY[{LEVEL}&GRAY] {PREFIX}%s &GOLD{SUFFIX}&GRAY&M%s";
+        String format = "&GRAY[{LEVEL}&GRAY] {PREFIX}%s &GOLD{SUFFIX}{GUILDSPACE}&GRAY&M%s";
         format = format
                 .replace("&GRAY", CC.GRAY.toString())
                 .replace("&GOLD", CC.YELLOW.toString())
@@ -81,7 +81,8 @@ public class Chat implements Listener {
                                 ? ""
                                 : rank.getPrefix() + " ")
                 )
-                .replace("{SUFFIX}", pw.getGuildTag() + " ");
+                .replace("{SUFFIX}", pw.getGuildTag())
+                .replace("{GUILDSPACE}", (pw.getGuildTag().equals("") ? "" : " "));
 
 
         for (Player h : Bukkit.getOnlinePlayers()) {
