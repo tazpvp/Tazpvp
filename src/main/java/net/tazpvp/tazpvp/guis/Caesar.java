f654d9f76b257b66705360373e421c1930363e32
rownox <rownox.ik@gmail.com>
2023-06-25 11:55:34 -0400

    caesar gui

--- src/main/java/net/tazpvp/tazpvp/guis/Caesar.java
+++ src/main/java/net/tazpvp/tazpvp/guis/Caesar.java
@@ -1,7 +1,6 @@
 package net.tazpvp.tazpvp.guis;
 
 import net.tazpvp.tazpvp.Tazpvp;
-import net.tazpvp.tazpvp.guis.guild.GuildBrowser;
 import net.tazpvp.tazpvp.utils.enums.CC;
 import net.tazpvp.tazpvp.utils.functions.BlockFunctions;
 import net.tazpvp.tazpvp.utils.functions.PlayerFunctions;
@@ -9,20 +8,14 @@ import net.tazpvp.tazpvp.utils.objects.Pickaxe;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 import world.ntdi.nrcore.utils.gui.Button;
 import world.ntdi.nrcore.utils.gui.GUI;
 import world.ntdi.nrcore.utils.item.builders.ItemBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Caesar extends GUI {
 
-    private static final List<Player> doubleClick = new ArrayList<>();
     public Caesar(Player p) {
         super("Caesar", 3);
         addItems(p);
@@ -30,55 +23,60 @@ public class Caesar extends GUI {
     }
 
     private void addItems(Player p) {
-        fill(0, 3*9, ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE, 1).name(" ").build());
+        fill(0, 27, ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE, 1).name(" ").build());
 
         ItemStack tool = BlockFunctions.getPickaxe(p);
         int shardCount = PlayerFunctions.countShards(p);
 
-        addButton(Button.create(ItemBuilder.of(tool.getType(), 1).name(CC.GREEN + "" + CC.BOLD + "Your Pickaxe").lore(CC.GRAY + "Click to upgrade its material.").flag(ItemFlag.HIDE_ATTRIBUTES).build(), (e) -> {
-            if (tool.getType().equals(Material.GOLDEN_PICKAXE)) {
-                p.sendMessage("You already have the best upgrade.");
+        int upgradeButtonSlot = 10;
+
+        if (tool.getType() == Material.GOLDEN_PICKAXE) {
+            sendNPCMessage(p, "&cCaesar", "You already have the best upgrade.");
+            return;
+        }
+
+        Pickaxe currentPickaxe = BlockFunctions.pickaxes.stream()
+                .filter(pickaxe -> pickaxe.getMat() == tool.getType())
+                .findFirst()
+                .orElse(null);
+
+        if (currentPickaxe == null) {
+            return;
+        }
+
+        int cost = currentPickaxe.getCost();
+
+        ItemBuilder upgradeButtonBuilder = ItemBuilder.of(tool.getType(), 1)
+                .name(CC.GREEN + "" + CC.BOLD + "Your Pickaxe")
+                .lore(CC.GRAY + "Click to upgrade its material. Cost: " + cost + " Shards")
+                .flag(ItemFlag.HIDE_ATTRIBUTES);
+
+        ItemStack upgradeButton = upgradeButtonBuilder.build();
+
+        addButton(Button.create(upgradeButton, (e) -> {
+            if (shardCount < cost) {
+                sendNPCMessage(p, "&cCaesar", "You do not have enough shards. You need " + (cost - shardCount) + " more shards.");
                 return;
             }
 
-            for (Pickaxe pickaxe : BlockFunctions.pickaxes) {
-                if (pickaxe.getMat().equals(tool.getType())) {
-                    int cost = pickaxe.getCost();
-                    if (shardCount >= cost) {
-                        if (doubleClick.contains(p)) {
+            tool.setType(currentPickaxe.getUpgrade());
+            PlayerFunctions.takeShards(p, cost);
 
-                            tool.setType(pickaxe.getUpgrade());
+            sendNPCMessage(p, "&cCaesar", "Thanks, here is your new pickaxe.");
+            p.playSound(p.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
 
-                            PlayerFunctions.takeShards(p, cost);
-                            doubleClick.remove(p);
+            Tazpvp.getObservers().forEach(observer -> observer.talent(p));
 
-                            p.closeInventory();
-                            p.sendMessage("Thanks, here is your new pickaxe.");
-                            p.playSound(p.getLocation(), Sound.BLOCK_ANVIL_USE, 1, 1);
+            upgradeButtonBuilder.lore(CC.GRAY + "Click to upgrade its material. Cost: " + cost + " Shards");
+            upgradeButtonBuilder.build();
 
-                            new Caesar(p);
-
-                            Tazpvp.getObservers().forEach(observer -> observer.talent(p));
-
-                        } else {
-
-                            p.sendMessage("Are you sure you want to upgrade your pickaxe for " + cost + " Shards?");
-
-                            doubleClick.add(p);
-                            new BukkitRunnable() {
-                                @Override
-                                public void run() {
-                                    doubleClick.remove(p);
-                                }
-                            }.runTaskLater(Tazpvp.getInstance(), 20*5);
-                        }
-                    } else {
-                        p.sendMessage("You do not have enough shards. You need " + (cost - shardCount) + " more shards.");
-                    }
-                }
-            }
-        }), 10);
+            update();
+        }), upgradeButtonSlot);
 
         update();
+    }
+
+    private void sendNPCMessage(Player player, String npcName, String message) {
+        player.sendMessage(CC.DARK_GRAY + "[" + npcName + "] " + CC.GRAY + message);
     }
 }
