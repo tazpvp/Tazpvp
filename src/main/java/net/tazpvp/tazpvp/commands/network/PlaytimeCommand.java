10cc10755991a069628b572da045be7d70bb862b
rownox <rownox.ik@gmail.com>
2023-09-08 23:47:26 -0400

    playtime command

--- src/main/java/net/tazpvp/tazpvp/commands/network/PlaytimeCommand.java
+++ src/main/java/net/tazpvp/tazpvp/commands/network/PlaytimeCommand.java
@@ -4,6 +4,9 @@ import lombok.NonNull;
 import net.tazpvp.tazpvp.utils.PlaytimeUtil;
 import net.tazpvp.tazpvp.utils.data.DataTypes;
 import net.tazpvp.tazpvp.utils.data.PersistentData;
+import net.tazpvp.tazpvp.utils.enums.CC;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +16,6 @@ import world.ntdi.nrcore.utils.command.simple.NRCommand;
 public class PlaytimeCommand extends NRCommand {
     public PlaytimeCommand() {
         super(new Label("playtime", null, "pt"));
-
     }
 
     @Override
@@ -23,11 +25,18 @@ public class PlaytimeCommand extends NRCommand {
             return true;
         }
 
-        int playtimeUnix = PersistentData.getInt(p.getUniqueId(), DataTypes.PLAYTIMEUNIX);
+        OfflinePlayer target;
+
+        if (args.length > 0)
+            target = Bukkit.getOfflinePlayer(args[0]);
+        else
+            target = p;
+
+        int playtimeUnix = PersistentData.getInt(target.getUniqueId(), DataTypes.PLAYTIMEUNIX);
         long playtimeSeconds = playtimeUnix / 1000;
         String formattedPlaytime = PlaytimeUtil.secondsToDDHHMMSS(playtimeSeconds);
 
-        p.sendMessage("Your total playtime: " + formattedPlaytime);
+        p.sendMessage(CC.GOLD + "" + CC.BOLD + target.getName() + "'s Playtime: " + CC.YELLOW + formattedPlaytime);
 
         return true;
     }
