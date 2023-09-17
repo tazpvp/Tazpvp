293c77b85954ad9b1b58944c94777b2f51a73761
Ntdi <yogikngyt@gmail.com>
2023-09-17 19:53:15 -0400

    change timings

--- src/main/java/net/tazpvp/tazpvp/utils/objects/bosses/BossManager.java
+++ src/main/java/net/tazpvp/tazpvp/utils/objects/bosses/BossManager.java
@@ -40,7 +40,7 @@ public class BossManager {
                     }
                 }
             }
-        }.runTaskTimer(p_javaPlugin, 3 * 20, 3 * 20);
+        }.runTaskTimer(p_javaPlugin, 5 * 20, 5 * 20);
     }
 
     public static void despawnBoss() {
