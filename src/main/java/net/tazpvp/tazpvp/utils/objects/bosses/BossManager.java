f439eb877306709761f2a3e51d6a87fb903ea638
rownox <rownox.ik@gmail.com>
2023-09-17 19:20:53 -0400

    h

--- src/main/java/net/tazpvp/tazpvp/utils/objects/bosses/BossManager.java
+++ src/main/java/net/tazpvp/tazpvp/utils/objects/bosses/BossManager.java
@@ -40,7 +40,7 @@ public class BossManager {
                     }
                 }
             }
-        }.runTaskTimer(p_javaPlugin, 1 * 20, 1 * 20);
+        }.runTaskTimer(p_javaPlugin, 3 * 20, 3 * 20);
     }
 
     public static void despawnBoss() {
