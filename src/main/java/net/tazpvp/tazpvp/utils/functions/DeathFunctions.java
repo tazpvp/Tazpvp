d3d4759e27f6cb0f608ff2721af509dbb35c7f29
Ntdi <yogikngyt@gmail.com>
2023-12-09 17:56:05 -0500

    rrr

--- src/main/java/net/tazpvp/tazpvp/utils/functions/DeathFunctions.java
+++ src/main/java/net/tazpvp/tazpvp/utils/functions/DeathFunctions.java
@@ -144,18 +144,16 @@ public class DeathFunctions {
         pVictim.getInventory().clear();
 
         final KitService kitService = new KitServiceImpl();
-        final KitEntity kitEntity = kitService.getKitEntity(pVictim.getUniqueId());
+        final KitEntity kitEntity = kitService.getOrDefault(pVictim.getUniqueId());
 
-        if (kitEntity != null) {
-            final String kitSerial = kitEntity.getSerial();
-            if (kitSerial == null || kitSerial.isEmpty()) {
-                PlayerFunctions.kitPlayer(pVictim);
-            } else {
-                SerializableInventory serializableInventory = SerializableInventory.convertFromString(kitSerial);
-                serializableInventory.addItems(pVictim.getInventory(), PlayerFunctions.getKitItems(pVictim));
+        final String kitSerial = kitEntity.getSerial();
+        if (kitSerial == null || kitSerial.isEmpty()) {
+            PlayerFunctions.kitPlayer(pVictim);
+        } else {
+            SerializableInventory serializableInventory = SerializableInventory.convertFromString(kitSerial);
+            serializableInventory.addItems(pVictim.getInventory(), PlayerFunctions.getKitItems(pVictim));
 
-                PlayerFunctions.armorPlayer(pVictim);
-            }
+            PlayerFunctions.armorPlayer(pVictim);
         }
 
         PlayerFunctions.resetHealth(pVictim);
