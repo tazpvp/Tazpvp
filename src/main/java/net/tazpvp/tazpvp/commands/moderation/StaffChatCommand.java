038157369a48da775fd8f3970e7213a4a0d96379
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-12-19 09:40:31 -0700

    add chat argument to staff chat

--- src/main/java/net/tazpvp/tazpvp/commands/moderation/StaffChatCommand.java
+++ src/main/java/net/tazpvp/tazpvp/commands/moderation/StaffChatCommand.java
@@ -3,9 +3,11 @@ package net.tazpvp.tazpvp.commands.moderation;
 import lombok.NonNull;
 import net.tazpvp.tazpvp.utils.enums.CC;
 import net.tazpvp.tazpvp.utils.player.PlayerWrapper;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import world.ntdi.nrcore.utils.ChatUtils;
 import world.ntdi.nrcore.utils.command.simple.Label;
 import world.ntdi.nrcore.utils.command.simple.NRCommand;
 
@@ -30,13 +32,23 @@ public class StaffChatCommand extends NRCommand {
 
         PlayerWrapper pw = PlayerWrapper.getPlayer(player);
 
-        // Toggle the staff chat state
-        pw.setStaffChatActive(!pw.isStaffChatActive());
+        if (args.length > 0) {
+            // Send the message to staff chat directly
+            final String message = ChatUtils.builder(args, 0);
 
-        // Notify the player about the staff chat state
-        player.sendMessage(CC.GREEN + "Staff chat " +
-                (pw.isStaffChatActive() ? "enabled" : "disabled"));
+            for (Player plr : Bukkit.getOnlinePlayers()) {
+                if (plr.hasPermission("tazpvp.staffchat")) {
+                    plr.sendMessage(CC.YELLOW.toString() + CC.BOLD + "Staff> " + player.getName() + CC.WHITE + " " + message);
+                }
+            }
+        } else {
+            // Toggle the staff chat state
+            pw.setStaffChatActive(!pw.isStaffChatActive());
 
+            // Notify the player about the staff chat state
+            player.sendMessage(CC.GREEN + "Staff chat " +
+                    (pw.isStaffChatActive() ? "enabled" : "disabled"));
+        }
         return true;
     }
 }
