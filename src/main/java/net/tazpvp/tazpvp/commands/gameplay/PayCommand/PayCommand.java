c3e37a34f10a71bfca761661a02e8755c5209073
juniorbutyeah <juniorbutyeah@gmail.com>
2024-07-21 16:48:27 -0400

    make pay command not so bad

--- src/main/java/net/tazpvp/tazpvp/commands/gameplay/PayCommand/PayCommand.java
+++ src/main/java/net/tazpvp/tazpvp/commands/gameplay/PayCommand/PayCommand.java
@@ -1,57 +1,73 @@
 package net.tazpvp.tazpvp.commands.gameplay.PayCommand;
 
+import lombok.NonNull;
 import net.tazpvp.tazpvp.data.DataTypes;
 import net.tazpvp.tazpvp.data.PersistentData;
+import net.tazpvp.tazpvp.utils.enums.CC;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import world.ntdi.nrcore.utils.command.simple.Completer;
 import world.ntdi.nrcore.utils.command.simple.Label;
 import world.ntdi.nrcore.utils.command.simple.NRCommand;
+
+import java.util.List;
 
 public class PayCommand extends NRCommand {
 
     public PayCommand() {
-        super(new Label("pay", "tazpvp.pay"));
+        super(new Label("pay", null));
     }
 
-    public boolean onExecute(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
+    @Override
+    public boolean execute(@NonNull CommandSender sender, @NonNull String[] args) {
+
+        if (!(sender instanceof Player p)) {
             sender.sendMessage("Only players can use this command.");
             return true;
         }
 
-        Player player = (Player) sender;
-
         if (args.length != 2) {
-            player.sendMessage("Usage: /pay <player> <amount>");
+            sendIncorrectUsage(sender, "/" + super.getLabel() + " <user> <amount>");
             return true;
         }
 
-        String targetName = args[0];
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            p.sendMessage(CC.RED + "Player not found.");
+            return true;
+        }
+
         int amount;
         try {
             amount = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            player.sendMessage("Invalid amount. Please enter a number.");
+            p.sendMessage(CC.RED + "Invalid amount.");
             return true;
         }
 
-        player.sendMessage("You paid " + targetName + " $" + amount);
-        Player playerTarget = Bukkit.getPlayer(targetName);
-        if (playerTarget == null) {
-
+        if (amount < 1) {
+            p.sendMessage(CC.RED + "Amount must be greater than 0.");
             return true;
         }
-        if (PersistentData.getInt(player.getUniqueId(), DataTypes.COINS) >= amount) {
-            PersistentData.add(playerTarget, DataTypes.COINS, amount);
 
-            PersistentData.remove(player.getUniqueId(), DataTypes.COINS, amount);
+        if (PersistentData.getInt(p, DataTypes.COINS) >= amount) {
+            PersistentData.remove(p, DataTypes.COINS, amount);
+            PersistentData.add(target, DataTypes.COINS, amount);
+            p.sendMessage(CC.GREEN + "You have paid " + target.getName() + " " + amount + " coins.");
+            target.sendMessage(CC.GREEN + p.getName() + " has paid you " + amount + " coins.");
+        } else {
+            p.sendMessage(CC.RED + "You do not have enough coins.");
             return true;
-
         }
-
-        return false;
+        return true;
     }
 
+    @Override
+    public List<String> complete(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            return Completer.onlinePlayers(args[0]);
+        }
+        return List.of();
+    }
 }
