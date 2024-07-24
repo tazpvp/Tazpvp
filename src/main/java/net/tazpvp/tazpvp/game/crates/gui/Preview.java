973787576f03f423350657dcd17a773508c2a53d
rownox <rownox.ik@gmail.com>
2024-07-24 08:38:22 -0400

    Crate previews

---
+++ src/main/java/net/tazpvp/tazpvp/game/crates/gui/Preview.java
@@ -0,0 +1,37 @@
+package net.tazpvp.tazpvp.game.crates.gui;
+
+import net.tazpvp.tazpvp.game.crates.Crate;
+import net.tazpvp.tazpvp.guis.Shop.Prestige;
+import net.tazpvp.tazpvp.utils.enums.CC;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import world.ntdi.nrcore.utils.gui.Button;
+import world.ntdi.nrcore.utils.gui.GUI;
+import world.ntdi.nrcore.utils.item.builders.ItemBuilder;
+
+public class Preview extends GUI {
+    private final Player player;
+    private final Crate crate;
+
+    public Preview(Player player, Crate crate) {
+        super("Crate Preview", 6);
+        this.player = player;
+        this.crate = crate;
+        addItems();
+    }
+
+    private void addItems() {
+        fill(0, 6*9, ItemBuilder.of(Material.BLACK_STAINED_GLASS_PANE, 1).name(" ").build());
+
+        int i = 0;
+        for (ItemStack item : crate.getCrateDrops()) {
+            addButton(Button.createBasic(item), i);
+            i++;
+        }
+
+        update();
+
+    }
+}
