c8b8a18f58b561812a07b788f49fd879b271938e
rownox <90977516+rownox@users.noreply.github.com>
2024-08-13 17:54:32 -0400

    ntdi doesnt like the code

--- src/main/java/net/tazpvp/tazpvp/data/services/GuildService.java
+++ src/main/java/net/tazpvp/tazpvp/data/services/GuildService.java
@@ -1,7 +1,28 @@
 package net.tazpvp.tazpvp.data.services;
 
+import com.j256.ormlite.dao.Dao;
+import net.tazpvp.tazpvp.data.DataService;
+import net.tazpvp.tazpvp.data.entity.GuildEntity;
+import net.tazpvp.tazpvp.data.entity.GuildMemberEntity;
+
+import java.util.List;
 import java.util.UUID;
 
-public interface GuildService {
-    boolean isInGuild(UUID uuid);
+public interface GuildService extends DataService {
+    Dao<GuildEntity, Integer> getUserDao();
+
+    GuildEntity createGuild(String name, UUID owner);
+    void saveGuild(GuildEntity guild);
+    void addMember(GuildEntity guild, UUID player, boolean officer);
+    GuildEntity getGuildByPlayer(UUID player);
+    GuildEntity getGuild(int id);
+    void deleteGuild(GuildEntity guild);
+    void messageAll(GuildEntity guild, String msg);
+    List<UUID> getAllOnlineMembers(GuildEntity guild);
+    List<UUID> getAllMembers(GuildEntity guild);
+    List<UUID> getOnlineOfficers(GuildEntity guild);
+    List<UUID> getOfficers(GuildEntity guild);
+    GuildMemberEntity getMemberEntity(GuildEntity guild, UUID uuid);
+    List<GuildEntity> getAllGuilds();
+    List<GuildEntity> getAllGuildsSorted();
 }
