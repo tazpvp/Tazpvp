b6d22a4793b18a25469bfc1bbf55797d1ff6df63
rownox <rownox.ik@gmail.com>
2024-08-19 10:42:21 -0400

    tournament initial

--- src/main/java/net/tazpvp/tazpvp/game/tournaments/Match.java
+++ src/main/java/net/tazpvp/tazpvp/game/tournaments/Match.java
@@ -1,4 +1,29 @@
 package net.tazpvp.tazpvp.game.tournaments;
 
+import net.tazpvp.tazpvp.objects.PartyObject;
+
+import java.util.List;
+
 public class Match {
+
+    private List<PartyObject> teams;
+
+
+    public Match(List<PartyObject> participants) {
+
+        this.teams = participants;
+    }
+
+
+    public void begin() {
+        for (PartyObject partyObject : teams) {
+            if (partyObject == null) {
+                end("One of the teams is absent.");
+            }
+        }
+    }
+
+    public void end(String msg) {
+
+    }
 }
--- src/main/java/net/tazpvp/tazpvp/game/tournaments/Tournament.java
+++ src/main/java/net/tazpvp/tazpvp/game/tournaments/Tournament.java
@@ -1,4 +1,54 @@
 package net.tazpvp.tazpvp.game.tournaments;
 
+import net.tazpvp.tazpvp.Tazpvp;
+import net.tazpvp.tazpvp.objects.PartyObject;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Tournament {
+    private final UUID host;
+    private final List<PartyObject> participants = new ArrayList<>();
+    private final List<Player> spectators = new ArrayList<>();
+    private final List<Match> bracket = new ArrayList<>();
+    private final int MATCH_NUMBER;
+
+    public Tournament(UUID host) {
+        this.host = host;
+        this.MATCH_NUMBER = 0;
+        initialize();
+    }
+
+    public void initialize() {
+
+    }
+
+    public void intermission() {
+        endCurrentMatch();
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                nextMatch();
+            }
+        }.runTaskLater(Tazpvp.getInstance(), 20*5);
+    }
+
+    public void endCurrentMatch() {
+        bracket.get(MATCH_NUMBER).end("Match ended");
+    }
+
+    public void nextMatch() {
+        bracket.get(MATCH_NUMBER).begin();
+    }
+
+    public void createBracket() {
+        int teamCount = participants.size();
+        if (teamCount % 2 != 0) {
+        }
+    }
+
 }
